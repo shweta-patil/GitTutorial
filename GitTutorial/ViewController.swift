2ceac40efa822564f08d6b5b9f0c8d6ae2b8db7f
Sweta <sweta.siddannawar@cognizant.com>
2018-07-20 16:36:33 +0530

    Initial Commit

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by Sweta Siddannawar on 7/20/18.
+//  Copyright © 2018 Sweta Siddannawar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
