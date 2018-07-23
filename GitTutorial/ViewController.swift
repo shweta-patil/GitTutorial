b2c0fbe72c3b4441080c7e702102b3e3c0a58c3e
Sweta <sweta.siddannawar@cognizant.com>
2018-07-23 12:55:01 +0530

    Another Print

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("Git Tutorial")
+        print("Add another print")
     }
-
 }
 
